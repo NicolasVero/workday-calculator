90421ad950082a229f549e77005650898f29df76
Nicolas Vero <nicolas.vero@univ-rouen.fr>
2025-08-20 20:06:30 +0200

    get hours

--- popup.js
+++ popup.js
@@ -1,53 +1,14 @@
-function parse_time_to_minutes(time_str) {
-	const [hours, minutes] = time_str.split(':').map(Number);
-	return hours * 60 + minutes;
-}
+document.getElementById("btn").addEventListener("click", async () => {
+	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-function minutes_to_time_string(minutes) {
-	const h = Math.floor(minutes / 60);
-	const m = minutes % 60;
-	return `${String(h).padStart(2, '0')}:${String(m).padStart(2, '0')}`;
-}
+	chrome.scripting.executeScript({
+		target: { tabId: tab.id },
+		func: () => {
+			const hours_span = document.querySelectorAll('#kgrdBadgeages tbody tr td:first-of-type span');
+			console.log(hours_span)
 
-function calculate_departure_time() {
-	const morning = document.getElementById('arrival_morning').value;
-	const lunch = document.getElementById('lunch_break').value;
-	const afternoon = document.getElementById('afternoon_return').value;
-
-	if (morning && lunch && afternoon) {
-		const morning_start = parse_time_to_minutes(morning);
-		const lunch_start = parse_time_to_minutes(lunch);
-		const afternoon_start = parse_time_to_minutes(afternoon);
-
-		const worked_before_lunch = lunch_start - morning_start;
-		const remaining_to_work = 444 - worked_before_lunch;
-
-		const departure_time = afternoon_start + remaining_to_work;
-		const departure_str = minutes_to_time_string(departure_time);
-
-		document.getElementById('result').textContent = `⏱️ Départ à ${departure_str}`;
-	} else {
-		document.getElementById('result').textContent = "";
-	}
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-	document.getElementById('arrival_morning').focus();
-	const inputs = document.querySelectorAll('input[type="time"]');
-
-	inputs.forEach((input, index) => {
-		input.addEventListener('change', calculate_departure_time);
-
-		input.addEventListener('keydown', (e) => {
-			if (e.key === 'Tab') {
-				e.preventDefault();
-				input.blur();
-
-				const nextInput = inputs[index + 1];
-				if (nextInput) {
-					nextInput.focus();
-				}
-			}
-		});
+			const hours = Array.from(hours_span).map(span => span.innerText);
+			console.log(hours)
+		}
 	});
 });
